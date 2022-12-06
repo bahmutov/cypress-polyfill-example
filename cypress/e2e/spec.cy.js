3371cbf8e8a871961954d99b51281cad98966318
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-12-05 22:58:08 -0500

    all tests

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,12 @@
 /// <reference types="cypress" />
 
 it('finds the last element', () => {
+  cy.visit('index.html')
+  cy.contains('button', 'Find').click()
+  cy.contains('output', '130')
+})
+
+it('finds the last element (older browser)', () => {
   cy.visit('index.html', {
     onBeforeLoad(win) {
       // simulate a browser that does not have
@@ -10,6 +16,27 @@ it('finds the last element', () => {
       delete win.Array.prototype.findLast
     },
   })
+  cy.contains('button', 'Find').click()
+  cy.contains('output', '130')
+})
+
+it('finds the last element (older browser, observe polyfill loads)', () => {
+  cy.intercept({
+    method: 'GET',
+    hostname: 'cdn.jsdelivr.net',
+    pathname: '/npm/core-js-bundle*/*',
+  }).as('core-js')
+  cy.visit('index.html', {
+    onBeforeLoad(win) {
+      // simulate a browser that does not have
+      // some ES6 Array methods
+      // If there is no polyfill, the app
+      // will throw an error trying to use it
+      delete win.Array.prototype.findLast
+    },
+  })
+  // confirm the polyfill loads
+  cy.wait('@core-js').its('response.statusCode').should('be.below', 400)
   cy.contains('button', 'Find').click()
   cy.contains('output', '130')
 })
